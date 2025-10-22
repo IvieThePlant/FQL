edecd3edef0fbba2bb13b48e71f5c0255077084c
IvieThePlant <=>
2025-10-22 14:41:10 -0500

    add save and delete methods

--- src/DatabaseModel.java
+++ src/DatabaseModel.java
@@ -3,7 +3,8 @@ package src;
 abstract class DatabaseModel<Model extends DatabaseModel<Model>> {
     /* TODO: need to implement:
      * [x] id stuff
-     * [ ] CRUD methods (save, delete) that call DatabaseHelper
+     * [x] CRUD methods (save, delete) that call DatabaseHelper
+     * [ ] helper() method to get DatabaseHelper instance
      * [ ] static methods for all, first, where that call DatabaseHelper
      * [ ] getColumns() to specify columns (excluding id)
      * [ ] toString() for display
@@ -11,4 +12,25 @@ abstract class DatabaseModel<Model extends DatabaseModel<Model>> {
 
     // id == 0 means "not yet persisted". Persisted records will get id > 0.
     public int id = 0;
+
+    // save(): add or update db based on id
+    public void save() {
+        DatabaseHelper<Model> h = helper();
+        if (this.id <= 0) {
+            h.add((Model) this);
+        } else {
+            h.update((Model) this);
+        }
+    }
+    // delete(): delete from db based on id
+    public void delete() {
+        DatabaseHelper<Model> h = helper();
+        if (this.id > 0) {
+            h.delete((Model) this);
+        }
+    }
+
+    private DatabaseHelper<Model> helper() {
+        // TODO
+    }
 }
