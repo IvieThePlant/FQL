df65c72872ddcce8b57c0aebd45b1ebfca733023
IvieThePlant <=>
2025-10-22 14:34:54 -0500

    setting unsaved model id as 0

--- src/DatabaseModel.java
+++ src/DatabaseModel.java
@@ -1,11 +1,14 @@
 package src;
 
-abstract class DatabaseModel {
+abstract class DatabaseModel<Model extends DatabaseModel<Model>> {
     /* TODO: need to implement:
-     * [ ] id stuff
-     * [ ] CRUD methods (save, delete)
-     * [ ] static methods for all, first, where
+     * [x] id stuff
+     * [ ] CRUD methods (save, delete) that call DatabaseHelper
+     * [ ] static methods for all, first, where that call DatabaseHelper
      * [ ] getColumns() to specify columns (excluding id)
      * [ ] toString() for display
      */
+
+    // id == 0 means "not yet persisted". Persisted records will get id > 0.
+    public int id = 0;
 }
