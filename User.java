2dfb24832b74422ad97ff158ff90cc643fd2258b
IvieThePlant <=>
2025-10-22 14:26:36 -0500

    optimistic example model

--- User.java
+++ User.java
@@ -1,13 +1,29 @@
-
-
 import src.DatabaseModel;
+// import java.util.HashMap; only needed if using the hashmap constructor
 
 public class User extends DatabaseModel<User> {
-    private String name;
-    private String email;
+    public String name;
+    public String email;
 
-    public String getName() {
-        return name;
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
 
+    /*These could be an option, but I want it to work without it too
+    public User(HashMap<String,String> params) {
+        this.name = params.get("name");
+        this.email = params.get("email");
+    }
+
+    public String[] getColumns() {
+        return new String[] { "name", "email" };
+    }
+
+    public String toString() {
+        return this.id + ", " + this.name + ", " + this.email;
+    }
+    */
 }
