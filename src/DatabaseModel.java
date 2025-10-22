13ac173209094f3aa7411a25d698c1f7a772dc31
IvieThePlant <=>
2025-10-22 14:32:28 -0500

    updating todo

--- src/DatabaseModel.java
+++ src/DatabaseModel.java
@@ -1,5 +1,11 @@
 package src;
 
 abstract class DatabaseModel {
-    // TODO: db stuff
+    /* TODO: need to implement:
+     * [ ] id stuff
+     * [ ] CRUD methods (save, delete)
+     * [ ] static methods for all, first, where
+     * [ ] getColumns() to specify columns (excluding id)
+     * [ ] toString() for display
+     */
 }
