d23fa6d6a5f33ecc9648d48205b4c80d50b6efdc
IvieThePlant <=>
2025-10-22 16:01:24 -0500

    i think that constructor looks good enough?

--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -1,10 +1,59 @@
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public final class DatabaseHelper<Model extends DatabaseModel<Model>> {
     /* TODO:
-     * [ ] constructor
+     * [x] constructor
      * [ ] read/write methods
      * [ ] CRUD stuff
      * [ ] first(), all(), where(), count(), clear()
      * [ ] db file name/path
      * [ ] CSV parsing
      */
+    private final Class<Model> modelClass;
+    private final String[] columnHeaders;
+    private final ArrayList<Model> records;
+
+    private final Path dbFile;
+    private static final String DB_DIR = "./database";
+
+    public DatabaseHelper(Class<Model> modelClass, String[] nonIdColumns) {
+        this.modelClass = modelClass;
+
+        // prepend "id" to columns
+        this.columnHeaders = new String[nonIdColumns.length + 1];
+        for (int i = 0; i < nonIdColumns.length; i++) {
+            if (i == 0) {
+                this.columnHeaders[0] = "id";
+            } else {
+                this.columnHeaders[i] = nonIdColumns[i];
+            }
+        }
+        
+        this.records = new ArrayList<>();
+        
+        // initialize db files
+        try { Files.createDirectories(Paths.get(DB_DIR)); } catch (IOException ignored) {} // create dir if doesnt exist yet
+        this.dbFile = Paths.get(DB_DIR, modelClass.getSimpleName() + ".csv");
+        try {
+            if (!Files.exists(this.dbFile)) {
+                Files.createFile(this.dbFile);
+                // write header line
+                String headerLine = String.join(",", this.columnHeaders) + "\n";
+                Files.write(this.dbFile, headerLine.getBytes());
+            } else {
+                // load existing records from file
+                loadFromFile();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to initialize database file for " + modelClass.getSimpleName(), e);
+        }
+    }
+
+    private void loadFromFile() {
+        // TODO
+    }
 }
